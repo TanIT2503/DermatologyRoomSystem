33939a237088ec5578f88e353ecfcf854210fa47
tanit2503 <congtan2503@gmail.com>
2022-09-22 09:45:07 +0700

    create account branch

--- src/main/java/com/cdio/dermatologroomsystem/controller/PatientRestController.java
+++ src/main/java/com/cdio/dermatologroomsystem/controller/PatientRestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/api/patient")
 public class PatientRestController {
